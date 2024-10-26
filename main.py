ad29281017883b30dfbf0f21ec708bd84ce42286
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-10-26 20:55:59 +0800

    Add charts

--- main.py
+++ main.py
@@ -21,12 +21,8 @@ def connect_to_db():
         user=os.environ.get('DB_USERNAME'),
         password=os.environ.get('DB_PASSWORD'),
         database="gamesdb"
-        # host="localhost",
-        # user="root",
-        # password="12345678",
-        # database="gamesdb",
-        # port=3000
     )
+
     return connection
 
 def get_genres_and_languages():
@@ -137,6 +133,47 @@ def main():
             )
             st.write(df)
 
+            match date_interval:
+                case 'yearly':
+                    year_count_df = df.data[['Year', 'count']].drop_duplicates().reset_index(drop=True)
+                    st.write("## Games Released per Year")
+                    st.bar_chart(
+                        year_count_df.set_index('Year'),
+                        x_label="Year",
+                        y_label="Count",
+                    )
+                    
+                case 'quarterly':
+                    year_quarter_count_df = df.data[['Year', 'Quarter', 'count']].drop_duplicates().reset_index(drop=True)
+                    year_quarter_count_df['Year-Quarter'] = year_quarter_count_df['Year'].astype(str) + ' Q' + year_quarter_count_df['Quarter'].astype(str)
+                    st.write("## Games Released per Quarter")
+                    st.bar_chart(
+                        year_quarter_count_df.set_index('Year-Quarter'),
+                        y="count",
+                        x_label="Year-Quarter",
+                        y_label="Count",
+                    )
+
+                case 'monthly':
+                    year_month_count_df = df.data[['Year', 'Month', 'count']].drop_duplicates().reset_index(drop=True)
+                    year_month_count_df['Year-Month'] = year_month_count_df['Year'].astype(str) + '-' + year_month_count_df['Month'].astype(str).str.zfill(2)
+                    st.write("## Games Released per Month")
+                    st.bar_chart(
+                        year_month_count_df.set_index('Year-Month'),
+                        y="count",
+                        x_label="Year-Month",
+                        y_label="Count",
+                    )
+            
+                case 'daily':
+                    date_count_df = df.data[['Date', 'count']].drop_duplicates().reset_index(drop=True)
+                    st.write("## Games Released per Day")
+                    st.bar_chart(
+                        date_count_df.set_index('Date'),
+                        y="count",
+                        y_label="Count",
+                    )
+
     elif procedure == "See how reviews and recommendations are affected by price range":
         price_interval = st.sidebar.number_input("Price Interval", min_value=10.0, step=0.1)
 
@@ -145,7 +182,26 @@ def main():
                 return df
 
             df = call_procedure(procedure, [price_interval], formatter)
+            df.data['avg_negative_reviews'] = df.data['avg_negative_reviews'].astype(float)
+            df.data['avg_positive_reviews'] = df.data['avg_positive_reviews'].astype(float)
+            df.data['avg_recommendations'] = df.data['avg_recommendations'].astype(float)
             st.write(df)
+            
+            st.write("## Reviews per Price Range")
+            st.bar_chart(
+                df.data,
+                y=["avg_positive_reviews", "avg_negative_reviews"],
+                x_label="Price Range",
+                y_label="Value",
+            )
+
+            st.write("## Recommendations per Price Range")
+            st.bar_chart(
+                df.data,
+                y=["avg_recommendations"],
+                x_label="Price Range",
+                y_label="Value",
+            )
 
     elif procedure == "See the relationship between genre and language to reviews and recommendations":
         selected_genres = st.sidebar.multiselect("Genres", genres, default=genres[0] if genres else None)
@@ -177,6 +233,21 @@ def main():
 
             df = call_procedure(procedure, [price_interval, pivot_axis], formatter)
             st.write(df)
+
+            if pivot_axis == "price":
+                st.write("## Developer Count per Price Range")
+                date_count_df = df.data.groupby('full_price_interval').agg({ "count": "sum" })
+                date_count_df = date_count_df.reset_index()
+                date_count_df['min_price'] = date_count_df['full_price_interval'].apply(lambda x: float(x.split('-')[0]))
+                date_count_df = date_count_df.sort_values(by='min_price').set_index('full_price_interval')
+                date_count_df = date_count_df.drop(columns=['min_price'])
+
+                st.bar_chart(
+                    date_count_df.reset_index(),
+                    y="count",
+                    x_label="Price Range",
+                    y_label="Count",
+                )
 
 if __name__ == "__main__":
     main()
